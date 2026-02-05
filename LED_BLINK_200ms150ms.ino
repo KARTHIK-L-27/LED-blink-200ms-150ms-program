2c8628053b6d5d088295d93db5b8516ca3750067
KARTHIK L <karthik9538kp@gmail.com>
2026-02-05 19:58:07 +0530

    Add files via upload

---
+++ LED_BLINK_200ms150ms.ino
@@ -0,0 +1,19 @@
+int led1= 7;
+int led2= 8;
+
+void setup() {
+ pinMode(led1,OUTPUT)
+ pinMode(led2,OUTPUT)
+}
+
+void loop() {
+  digitalWrite(led1,HIGH)
+  digitalWrite(led2,LOW)
+  delay(200)
+
+ digitalWrite(led1,LOW)
+ digitalWrite(led2,HIGH)
+ dealy(150)
+}
+
+
